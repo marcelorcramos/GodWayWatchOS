71ec295cc0705bfc9a557a4c845ac40b774ba592
Marcelo Ramos <marcelorcramos@gmail.com>
2026-02-21 10:33:27 +0000

    Initial Commit

---
+++ GodWay Watch App/GodWayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GodWayApp.swift
+//  GodWay Watch App
+//
+//  Created by Marcelo Ramos on 21/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GodWay_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
